6df6c3cf99a64ae165804121a559089a922f0056
Emiliano Cervantes <ovman96@hotmail.com>
2018-01-23 12:55:24 -0600

    Async Task created.

--- app/src/main/java/com/example/emilianocervantes/marvelapp/MainActivity.java
+++ app/src/main/java/com/example/emilianocervantes/marvelapp/MainActivity.java
@@ -5,33 +5,70 @@ import android.app.ListActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class MainActivity extends ListActivity {
+
+    private ListView listView;
 
     private ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Json json = new Json();
+        listView = (ListView)findViewById(R.id.Lista);
+        /*Json json = new Json();
         String jsonString = json.serviceCall("https://itunes.apple.com/search?term=maroon+5");
         arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,new ArrayList<String>());
 
         arrayAdapter.add(jsonString);
-        setListAdapter(arrayAdapter);
+        setListAdapter(arrayAdapter);*/
+
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,new ArrayList<String>());
+        listView.setAdapter(arrayAdapter);
+        //setListAdapter(arrayAdapter);
+
+        new ProcesaJSON(arrayAdapter).execute("https://itunes.apple.com/search?term=maroon+5");
     }
 
-    public class ProcesaJSON extends AsyncTask<Void,Integer,ArrayList<String>>{
-        private ArrayAdapter<String> arrayAdapter;
+    public class ProcesaJSON extends AsyncTask<String,Integer,ArrayList<String>>{
+        private ArrayAdapter<String> adapt;
         public ProcesaJSON(ArrayAdapter<String> adapter){
-            this.arrayAdapter = adapter;
+            this.adapt = adapter;
         }
 
         @Override
-        protected ArrayList<String> doInBackground(Void... voids) {
-            return null;
+        protected ArrayList<String> doInBackground(String... urls) {
+            //Se encarga de conectarse al servicio
+            Json json = new Json();
+            String jsonString = json.serviceCall(urls[0]);
+            ArrayList<String> arrayList = new ArrayList<>();
+            try {
+                JSONObject jsonObject = new JSONObject();
+                JSONArray jsonArray = jsonObject.getJSONArray("results");
+                for (int i = 0; i<jsonArray.length(); i++){
+                    JSONObject dato = jsonArray.getJSONObject(i);
+                    arrayList.add(dato.getString("collection"));
+                }
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
+
+            //arrayList.add(jsonString);
+            return arrayList;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> strings) {
+            arrayAdapter.clear();
+            arrayAdapter.addAll(strings);
+            arrayAdapter.notifyDataSetChanged();
         }
     }
 }
